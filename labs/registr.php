044dead1e956f6ef584c914bd8d758c42d300324
ZyabbarovArturIVTASbd-12 <98730843+ZyabbarovArturIVTASbd-12@users.noreply.github.com>
2022-05-06 23:43:24 +0400

    Add files via upload

---
+++ labs/registr.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Форма регистрации</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+    <div class="container mt-4">
+    <h1>Форма регистрации</h1>
+    <form action="" method="post">
+        <input type="text" class="form-control" name="login"
+        id="login" placeholder="Введите логин"><br>
+        <input type="text" class="form-control" name="name"
+        id="name" placeholder="Введите логин"><br>
+        <input type="password" class="form-control" name="pass"
+        id="pass" placeholder="Введите логин"><br>
+        <button class="btn btn-success" type="submit">Зарегистрировать</button>
+    </form>
+    </div>
+</body>
+</html>
